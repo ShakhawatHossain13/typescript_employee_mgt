38297a9d010d7b2d243800ce6e717df9b9e3f3aa
ShakhawatHossain13 <72038014+ShakhawatHossain13@users.noreply.github.com>
2022-08-11 09:32:18 +0600

    Update App.tsx

--- typescript_employee/src/App.tsx
+++ typescript_employee/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import EmployeeTable from './components/Employee/EmployeeTable';
 import AddEmployee from './components/Employee/AddEmployee';
+import EditEmployee from './components/Employee/EditEmployee';
 import Nav from './components/Nav';
 
 const App: React.FC=()=> {
@@ -13,6 +14,7 @@ const App: React.FC=()=> {
       <Routes>
           <Route path="/" element={<EmployeeTable />} />
           <Route path="/add" element={<AddEmployee />} />  
+          <Route path="/edit-employee/:id" element={<EditEmployee />} />  
 
       </Routes>    
     </React.Fragment>
