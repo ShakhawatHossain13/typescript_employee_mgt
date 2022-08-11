eb0a11864f7c104130504f3360468b4e8b787ee7
ShakhawatHossain13 <72038014+ShakhawatHossain13@users.noreply.github.com>
2022-08-11 17:42:51 +0600

    Update index.tsx

--- typescript_employee/src/components/Nav/index.tsx
+++ typescript_employee/src/components/Nav/index.tsx
@@ -8,8 +8,7 @@ const useStyles = makeStyles({
         margin: '0',
         padding: '0',
         overflow: 'hidden' ,
-        backgroundColor: 'cadetblue' ,
-      
+        backgroundColor: 'cadetblue',
     }, 
     item:{
         float: "right",
@@ -21,23 +20,20 @@ const useStyles = makeStyles({
         padding: '14px 16px',
         textDecoration: 'none',
     }
- 
   }
   );
- 
 
 const Nav:React.FC =()=>{
     const classes = useStyles();
     return(
         <React.Fragment>
-        <div>
-            <ul className={classes.list}>
-            <li  className={classes.item}><a  className={classes.link}  >Logout</a></li>          
-            <li  className={classes.item}><a className={classes.link} >Contact</a></li>
-            <li  className={classes.item}><Link to="/add" className={classes.link}>Add Employee</Link></li>
-            <li  className={classes.item}><Link to="/" className={classes.link}>Employee</Link></li>
-        </ul>
-
+            <div>
+                <ul className={classes.list}>
+                    <li  className={classes.item}><a  className={classes.link}  >Logout</a></li>          
+                    <li  className={classes.item}><a className={classes.link} >Contact</a></li>
+                    <li  className={classes.item}><Link to="/add" className={classes.link}>Add Employee</Link></li>
+                    <li  className={classes.item}><Link to="/" className={classes.link}>Employee</Link></li>
+                </ul>
             </div>
       
         </React.Fragment>
